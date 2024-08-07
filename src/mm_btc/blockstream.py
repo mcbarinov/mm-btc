0bf78dc27e0c0c5b9bfc28957590fec0c0450e87
mcbarinov <mcbarinov@proton.me>
2024-08-07 10:14:59 +0300

    wallet #2

--- src/mm_btc/blockstream.py
+++ src/mm_btc/blockstream.py
@@ -5,9 +5,11 @@ from mm_std import Err, Ok, Result, hr
 from mm_std.random_ import random_str_choice
 from pydantic import BaseModel
 
-BASE_URL = "https://blockstream.info/api"
+MAINNET_BASE_URL = "https://blockstream.info/api"
+TESTNET_BASE_URL = "https://blockstream.info/testnet/api"
 
 ERROR_INVALID_ADDRESS = "INVALID_ADDRESS"
+ERROR_INVALID_NETWORK = "INVALID_NETWORK"
 
 Proxy: TypeAlias = str | Sequence[str] | None
 
@@ -31,24 +33,31 @@ class Address(BaseModel):
     mempool_stats: MempoolStats
 
 
-def get_address(address: str, timeout: int = 10, proxies: Proxy = None, attempts: int = 1) -> Result[Address]:
+def get_address(
+    address: str, testnet: bool = False, timeout: int = 10, proxies: Proxy = None, attempts: int = 1
+) -> Result[Address]:
     result: Result[Address] = Err("not started yet")
     data = None
+    base_url = TESTNET_BASE_URL if testnet else MAINNET_BASE_URL
     for _ in range(attempts):
         try:
-            res = hr(f"{BASE_URL}/address/{address}", timeout=timeout, proxy=random_str_choice(proxies))
+            res = hr(f"{base_url}/address/{address}", timeout=timeout, proxy=random_str_choice(proxies))
             data = res.to_dict()
             if res.code == 400 and (
                 "invalid bitcoin address" in res.body.lower() or "bech32 segwit decoding error" in res.body.lower()
             ):
                 return Err(ERROR_INVALID_ADDRESS, data=data)
+            elif res.code == 400 and "invalid network" in res.body.lower():
+                return Err(ERROR_INVALID_NETWORK, data=data)
             return Ok(Address(**res.json))
         except Exception as err:
             result = Err(err, data=data)
     return result
 
 
-def get_confirmed_balance(address: str, timeout: int = 10, proxies: Proxy = None, attempts: int = 1) -> Result[int]:
-    return get_address(address, timeout=timeout, proxies=proxies, attempts=attempts).and_then(
+def get_confirmed_balance(
+    address: str, testnet: bool = False, timeout: int = 10, proxies: Proxy = None, attempts: int = 1
+) -> Result[int]:
+    return get_address(address, testnet=testnet, timeout=timeout, proxies=proxies, attempts=attempts).and_then(
         lambda a: Ok(a.chain_stats.funded_txo_sum - a.chain_stats.spent_txo_sum),
     )
